5a9b4d9867ce8cd8c541b54121f954709f2b7338
Parimarjan <pnegi@stanford.edu>
2016-12-03 23:06:54 -0800

    Added logs and images.

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@
 #PBS -q gpu
 #PBS -d .
 
-LAUNCHER='mpirun --bind-to none -np 2' regent.py ~/project/wavelet.rg -i \
-~/project/images/gates.png -o ~/project/test.png -ll:cpu 4 -ll:csize 1024 -hl:prof 1 \
--hl:spy -logfile wavelet_parallel
+LAUNCHER='mpirun --bind-to none -np 2' regent.py ~/wic/wavelet.rg -i \
+~/wic/images/gates.png -o ~/wic/test.png -ll:cpu 4 -ll:csize 1024 -hl:prof 1 \
+-hl:spy -logfile wavelet_%.log
 
  
 
