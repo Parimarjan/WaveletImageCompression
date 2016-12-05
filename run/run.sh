4c6f4d52b3de618f614d65bbfb9f3e68049afd52
Parimarjan <pnegi@stanford.edu>
2016-12-05 04:02:27 -0800

    Updated wavelet to include test assert.

--- run/run.sh
+++ run/run.sh
@@ -1,23 +1,21 @@
 #!/bin/bash -l 
-#PBS -l nodes=1:ppn=24
+#PBS -l nodes=4:ppn=24
 #PBS -l walltime=00:05:00
 #PBS -m abe
 #PBS -q gpu
 #PBS -d .
 
-# remove old images just in case.
-#rm ~/wic/*.png
-#rm ~/wic/logs/*
+rm ./*.png
+rm ./run.sh.*
+rm ./logs/*.log
 
-#LAUNCHER='mpirun --bind-to none -np 1' regent.py ~/wic/wavelet.rg -i \
-#~/wic/images/gates.png -ll:cpu 4 -ll:csize 1024
-
-#LAUNCHER='mpirun --bind-to none -np 4 -npernode 1' regent.py ~/wic/wavelet.rg -i \
-#~/wic/images/gates.png -ll:cpu 4 -ll:csize 2000 \
 
 LAUNCHER='mpirun --bind-to none -np 4 -npernode 1' regent.py ~/wic/wavelet.rg -i \
-~/wic/images/gates.png -ll:cpu 4 -ll:csize 32000 \
-#-hl:prof 1 -logfile ~/wic/logs/wavelet_%.log
+~/wic/images/gates.png -ll:cpu 8 -ll:csize 16000 \
+-hl:prof 4 -logfile ~/wic/run/logs/wavelet_%.log
+
+# Now we can generate legion prof files:
+#~/legion/tools/legion_prof.py wavelet_0.log
 
 # This will be with profiling + spy
 #LAUNCHER='mpirun --bind-to none -np 2 -npernode 1' regent.py ~/wic/wavelet.rg -i \
